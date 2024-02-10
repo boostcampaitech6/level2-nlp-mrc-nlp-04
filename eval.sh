8fe824a8415d32e0f9c4e0a2c00d63efb6b3353a
kooqooo <koo9717@gmail.com>
2024-02-10 16:19:43 +0900

    fix: shell script
    #2

--- eval.sh
+++ eval.sh
@@ -1,8 +1,8 @@
 python ./code/train.py \
---do_eval \
---per_device_train_batch_size 16 \ 
---per_device_eval_batch_size 16 \
---model_name_or_path ./models/train_dataset/ \
---output_dir ./outputs/train_dataset \
---overwrite_output_dir \
---overwrite_cache
+ --do_eval \
+ --per_device_train_batch_size 16 \
+ --per_device_eval_batch_size 16 \
+ --model_name_or_path ./models/train_dataset/ \
+ --output_dir ./outputs/train_dataset \
+ --overwrite_output_dir \
+ --overwrite_cache
