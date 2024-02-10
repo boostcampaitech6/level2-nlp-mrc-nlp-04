0f933ad19d21350b8e07461d853840c8682a0e63
kooqooo <koo9717@gmail.com>
2024-02-10 16:58:08 +0900

    docs: add readme
    * 실행방법에 대한 설명입니다.
    
    #2

--- eval.sh
+++ eval.sh
@@ -1,6 +1,5 @@
 python ./code/train.py \
  --do_eval \
- --per_device_train_batch_size 16 \
  --per_device_eval_batch_size 16 \
  --model_name_or_path ./models/train_dataset/ \
  --output_dir ./outputs/train_dataset \
--- inference.sh
+++ inference.sh
@@ -1,6 +1,6 @@
 python ./code/inference.py \
  --do_predic \
- --per_device_eval_batch_size 16 \
+ --per_device_eval_batch_size 32 \
  --output_dir ./outputs/test_dataset/ \
  --dataset_name ./data/test_dataset/ \
  --model_name_or_path ./models/train_dataset/ \
